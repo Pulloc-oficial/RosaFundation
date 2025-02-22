cb3bd313228deb6035c0b780192f3d3048067759
LyleRomero <lyle.planet@gmail.com>
2025-02-22 00:46:23 -0500

    Boceto de todos los componentes de la pagina sin incluir scripts

--- rosa-fundation/src/App.tsx
+++ rosa-fundation/src/App.tsx
@@ -1,11 +1,24 @@
 import { useState } from 'react'
 import './index.css'
+import Navbar from './components/navbar'
+import Hero from './components/hero'
+import Info from './components/info'
+import CardsSection from './components/cardSection'
+import PricingSection from './components/pricingSection'
+import CallToAction from './components/callToAction'
+import Footer from './components/footer'
 
 function App() {
   return (
-    <>
-      <div>APP</div>
-    </>
+    <div className="leading-normal tracking-normal text-white gradient" style={{ fontFamily: "'Source Sans Pro', sans-serif" }}>
+      <Navbar />
+      <Hero />
+      <Info />
+      <CardsSection />
+      <PricingSection />
+      <CallToAction /> 
+      <Footer />
+    </div>
   )
 }
 
