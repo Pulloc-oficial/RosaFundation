3cfca8d11bef68c2a882b67768453f918d1cd658
LyleRomero <lyle.planet@gmail.com>
2025-04-15 08:25:39 -0500

    Diseño footer

--- rosa-fundation/src/components/page1/home/contactSection.tsx
+++ rosa-fundation/src/components/page1/home/contactSection.tsx
@@ -1,17 +1,17 @@
 // src/components/ContactSection.tsx
-import React from 'react';
-import { FaFacebookF, FaInstagram, FaYoutube, FaTwitter } from 'react-icons/fa';
 
+import { FaFacebookF, FaInstagram } from 'react-icons/fa';
+import { SiX } from "react-icons/si";
 
 const ContactSection = () => {
   return (
-    <div className="relative flex items-top justify-center min-h-screen bg-white dark:bg-gray-900 sm:items-center sm:pt-0">
+    <div className="font-[Montserrat] text-[#004B5A] relative flex items-top justify-center min-h-screen bg-white dark:bg-gray-900 sm:items-center sm:pt-0">
       <div className="max-w-6xl mx-auto sm:px-6 lg:px-8 w-full">
         <div className="mt-8 overflow-hidden">
           <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
             {/* Info Column */}
             <div className="p-6 mr-2 bg-gray-100 dark:bg-gray-800 sm:rounded-lg">
-              <h1 className="text-4xl sm:text-5xl text-gray-800 dark:text-white font-extrabold tracking-tight">
+              <h1 className="text-4xl sm:text-5xl text-gray-800 dark:text-white font-extrabold tracking-tight ">
                 Contactanos
               </h1>
               <p className="text-lg sm:text-2xl font-medium text-gray-600 dark:text-gray-400 mt-2">
@@ -19,29 +19,23 @@ const ContactSection = () => {
               </p>
 
               <div className="flex items-center mt-8 text-gray-600 dark:text-gray-400">
-                <svg className="w-8 h-8 text-gray-500" fill="none" stroke="currentColor" strokeWidth={1.5} viewBox="0 0 24 24">
-                <FaYoutube />
-                </svg>
+                <FaInstagram />
                 <div className="ml-4 text-md tracking-wide font-semibold w-40">
-                  Acme Inc, Street, State, Postal Code
+                  @FundacionRosa
                 </div>
               </div>
 
               <div className="flex items-center mt-4 text-gray-600 dark:text-gray-400">
-                <svg className="w-8 h-8 text-gray-500" fill="none" stroke="currentColor" strokeWidth={1.5} viewBox="0 0 24 24">
                 <FaFacebookF />
-                </svg>
                 <div className="ml-4 text-md tracking-wide font-semibold w-40">
-                  +44 1234567890
+                  @FundacionRosa
                 </div>
               </div>
 
-              <div className="flex items-center mt-2 text-gray-600 dark:text-gray-400">
-                <svg className="w-8 h-8 text-gray-500" fill="none" stroke="currentColor" strokeWidth={1.5} viewBox="0 0 24 24">
-                <FaTwitter />
-                </svg>
+              <div className="flex items-center mt-4 text-gray-600 dark:text-gray-400">
+                <SiX />
                 <div className="ml-4 text-md tracking-wide font-semibold w-40">
-                  info@acme.org
+                  FundacionRosa
                 </div>
               </div>
             </div>
@@ -83,7 +77,7 @@ const ContactSection = () => {
 
               <button
                 type="submit"
-                className="bg-indigo-600 hover:bg-indigo-500 text-white font-bold py-3 px-6 rounded-lg transition ease-in-out duration-300 w-full md:w-32"
+                className="bg-[#57D700] hover:bg-[#4BC000] text-white font-semibold py-3 px-6 rounded-lg transition ease-in-out duration-300 w-full md:w-32"
               >
                 Submit
               </button>
--- rosa-fundation/src/components/page1/home/footer.tsx
+++ rosa-fundation/src/components/page1/home/footer.tsx
@@ -2,71 +2,119 @@ import React from "react";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="bg-white">
-      <div className="container mx-auto px-8">
-        <div className="w-full flex flex-col md:flex-row py-6">
-          <div className="flex-1 mb-6 text-black">
-            <a className="text-pink-600 no-underline hover:no-underline font-bold text-2xl lg:text-4xl" href="#">
-              Fundacion Rosa
+<div className="font-[Montserrat] flex items-end w-full min-h-screen bg-white">
+
+<footer className="w-full text-gray-700 bg-gray-100 body-font">
+    <div
+        className="container flex flex-col flex-wrap px-5 py-24 mx-auto md:items-center lg:items-start md:flex-row md:flex-no-wrap">
+        <div className ="flex-shrink-0 w-64 mx-auto text-center md:mx-0 md:text-left">
+            <a className ="flex items-center justify-center font-medium text-gray-900 title-font md:justify-start">
+            <img src="../images/logo.png" alt="logo fundacion rosa" className="h-15 inline"/> 
             </a>
-          </div>
-          <div className="flex-1">
-            <p className="uppercase text-gray-500 md:mb-6">Links</p>
-            <ul className="list-reset mb-6">
-              <li className="mt-2 inline-block mr-2 md:block md:mr-0">
-                <a href="#" className="no-underline hover:underline text-gray-800 hover:text-pink-500">FAQ</a>
-              </li>
-              <li className="mt-2 inline-block mr-2 md:block md:mr-0">
-                <a href="#" className="no-underline hover:underline text-gray-800 hover:text-pink-500">Help</a>
-              </li>
-              <li className="mt-2 inline-block mr-2 md:block md:mr-0">
-                <a href="#" className="no-underline hover:underline text-gray-800 hover:text-pink-500">Support</a>
-              </li>
-            </ul>
-          </div>
-          <div className="flex-1">
-            <p className="uppercase text-gray-500 md:mb-6">Legal</p>
-            <ul className="list-reset mb-6">
-              <li className="mt-2 inline-block mr-2 md:block md:mr-0">
-                <a href="#" className="no-underline hover:underline text-gray-800 hover:text-pink-500">Terms</a>
-              </li>
-              <li className="mt-2 inline-block mr-2 md:block md:mr-0">
-                <a href="#" className="no-underline hover:underline text-gray-800 hover:text-pink-500">Privacy</a>
-              </li>
-            </ul>
-          </div>
-          <div className="flex-1">
-            <p className="uppercase text-gray-500 md:mb-6">Social</p>
-            <ul className="list-reset mb-6">
-              <li className="mt-2 inline-block mr-2 md:block md:mr-0">
-                <a href="#" className="no-underline hover:underline text-gray-800 hover:text-pink-500">Facebook</a>
-              </li>
-              <li className="mt-2 inline-block mr-2 md:block md:mr-0">
-                <a href="#" className="no-underline hover:underline text-gray-800 hover:text-pink-500">Linkedin</a>
-              </li>
-              <li className="mt-2 inline-block mr-2 md:block md:mr-0">
-                <a href="#" className="no-underline hover:underline text-gray-800 hover:text-pink-500">Twitter</a>
-              </li>
-            </ul>
-          </div>
-          <div className="flex-1">
-            <p className="uppercase text-gray-500 md:mb-6">Company</p>
-            <ul className="list-reset mb-6">
-              <li className="mt-2 inline-block mr-2 md:block md:mr-0">
-                <a href="#" className="no-underline hover:underline text-gray-800 hover:text-pink-500">Official Blog</a>
-              </li>
-              <li className="mt-2 inline-block mr-2 md:block md:mr-0">
-                <a href="#" className="no-underline hover:underline text-gray-800 hover:text-pink-500">About Us</a>
-              </li>
-              <li className="mt-2 inline-block mr-2 md:block md:mr-0">
-                <a href="#" className="no-underline hover:underline text-gray-800 hover:text-pink-500">Contact</a>
-              </li>
-            </ul>
-          </div>
+            <p className ="mt-2 text-sm text-gray-500">Centro campestre de rehabilitación para adictos</p>
+            <div className="mt-4">
+                <span className="inline-flex justify-center mt-2 sm:ml-auto sm:mt-0 sm:justify-start">
+                    <a className="text-gray-500 cursor-pointer hover:text-gray-700">
+                        <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            className="w-5 h-5" viewBox="0 0 24 24">
+                            <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+                        </svg>
+                    </a>
+                    <a className="ml-3 text-gray-500 cursor-pointer hover:text-gray-700">
+                        <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            className="w-5 h-5" viewBox="0 0 24 24">
+                            <path
+                                d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z">
+                            </path>
+                        </svg>
+                    </a>
+                    <a className="ml-3 text-gray-500 cursor-pointer hover:text-gray-700">
+                        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                            stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
+                            <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
+                            <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+                        </svg>
+                    </a>
+                    <a className="ml-3 text-gray-500 cursor-pointer hover:text-gray-700">
+                        <svg fill="currentColor" stroke="currentColor" stroke-linecap="round"
+                            stroke-linejoin="round" stroke-width="0" className="w-5 h-5" viewBox="0 0 24 24">
+                            <path stroke="none"
+                                d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z">
+                            </path>
+                            <circle cx="4" cy="4" r="2" stroke="none"></circle>
+                        </svg>
+                    </a>
+                </span>
+            </div>
         </div>
+        <div className="flex flex-wrap flex-grow mt-10 -mb-10 text-center md:pl-20 md:mt-0 md:text-left">
+            <div className="w-full px-4 lg:w-1/4 md:w-1/2">
+                <h2 className="mb-3 text-sm font-medium tracking-widest text-gray-900 uppercase title-font">About</h2>
+                <nav className="mb-10 list-none">
+                    <li className="mt-3">
+                        <a className="text-gray-500 cursor-pointer hover:text-gray-900">Company</a>
+                    </li>
+                    <li className="mt-3">
+                        <a className="text-gray-500 cursor-pointer hover:text-gray-900">Careers</a>
+                    </li>
+                    <li className="mt-3">
+                        <a className="text-gray-500 cursor-pointer hover:text-gray-900">Blog</a>
+                    </li>
+                </nav>
+            </div>
+            <div className="w-full px-4 lg:w-1/4 md:w-1/2">
+                <h2 className="mb-3 text-sm font-medium tracking-widest text-gray-900 uppercase title-font">Support</h2>
+                <nav className="mb-10 list-none">
+                    <li className="mt-3">
+                        <a className="text-gray-500 cursor-pointer hover:text-gray-900">Contact Support</a>
+                    </li>
+                    <li className="mt-3">
+                        <a className="text-gray-500 cursor-pointer hover:text-gray-900">Help Resources</a>
+                    </li>
+                    <li className="mt-3">
+                        <a className="text-gray-500 cursor-pointer hover:text-gray-900">Release Updates</a>
+                    </li>
+                </nav>
+            </div>
+            <div className="w-full px-4 lg:w-1/4 md:w-1/2">
+                <h2 className="mb-3 text-sm font-medium tracking-widest text-gray-900 uppercase title-font">Platform
+                </h2>
+                <nav className="mb-10 list-none">
+                    <li className="mt-3">
+                        <a className="text-gray-500 cursor-pointer hover:text-gray-900">Terms &amp; Privacy</a>
+                    </li>
+                    <li className="mt-3">
+                        <a className="text-gray-500 cursor-pointer hover:text-gray-900">Pricing</a>
+                    </li>
+                    <li className="mt-3">
+                        <a className="text-gray-500 cursor-pointer hover:text-gray-900">FAQ</a>
+                    </li>
+                </nav>
+            </div>
+            <div className="w-full px-4 lg:w-1/4 md:w-1/2">
+                <h2 className="mb-3 text-sm font-medium tracking-widest text-gray-900 uppercase title-font">Contact</h2>
+                <nav className="mb-10 list-none">
+                    <li className="mt-3">
+                        <a className="text-gray-500 cursor-pointer hover:text-gray-900">Send a Message</a>
+                    </li>
+                    <li className="mt-3">
+                        <a className="text-gray-500 cursor-pointer hover:text-gray-900">Request a Quote</a>
+                    </li>
+                    <li className="mt-3">
+                        <a className="text-gray-500 cursor-pointer hover:text-gray-900">+57 311-123-4567</a>
+                    </li>
+                </nav>
+            </div>
+        </div>
+    </div>
+      <div className="bg-gray-300">
+          <div className="container px-5 py-4 mx-auto">
+              <p className="text-sm text-gray-700 capitalize xl:text-center">© 2020 All rights reserved - desarrollado por pulloc</p>
+          </div>
       </div>
-      <a href="https://www.freepik.com/free-photos-vectors/background" className="text-gray-500">Background vector created by freepik - www.freepik.com</a>
-    </footer>
+</footer>
+
+</div>
   );
 };
 
